6f8e7115c009f8c5bfbc2ae350f093df2e45a106
maxiflores <maxir_flores@hotmail.com>
2019-03-30 17:18:37 -0300

    add files

---
+++ NetCoreApp/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using NetCoreApp.Models;
+using Newtonsoft.Json;
+
+namespace NetCoreApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public HomeController(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            string contentRootPath = _hostingEnvironment.ContentRootPath;
+            var json= System.IO.File.ReadAllText(contentRootPath + "/Data.json");
+
+            ListProducts lst= JsonConvert.DeserializeObject<ListProducts>(json);
+
+            return View(lst);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ NetCoreApp/Models/ProductosViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreApp.Models
+{
+    public class ProductosViewModel
+    {
+        public string name { get; set; }
+        public string price { get; set; }
+    }
+
+    public class ListProducts
+    {
+       public List<ProductosViewModel> products { get; set; }
+    }
+}
